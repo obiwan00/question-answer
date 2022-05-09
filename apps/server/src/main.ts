25ff7331a959bd0e785ca7e619306840c70ffda4
Ivan Maidaniuk <ivan.maidaniuk@gmail.com>
2022-05-09 23:24:44 +0300

    Add swagger

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,7 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 
@@ -16,6 +16,14 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(globalPrefix);
   app.enableShutdownHooks();
+
+  const config = new DocumentBuilder()
+    .setTitle('Question Answers API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
